fa70a42d48ecaac7f2ca5f689dcbe03f5e0430e7
Pavel Anpleenko <19paul91@mail.ru>
2021-01-08 13:19:28 +0300

    Completed work with form which create a new user

--- Kmeza/Supporting Files/SceneDelegate.swift
+++ Kmeza/Supporting Files/SceneDelegate.swift
@@ -24,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //		}
 		
 		let currentRootViewController = FetchRootViewController.fetchData()
-		
+
 		if currentRootViewController == "HomeScreenViewController" {
 			ShowViewScreen.shared.showHomeScreen()
 		} else if currentRootViewController == "WelcomeViewController" {
