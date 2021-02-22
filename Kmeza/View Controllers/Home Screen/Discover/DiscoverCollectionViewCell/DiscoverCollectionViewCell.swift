d1d14128760769dc007f0ca51200557b48944684
allakin <19paul91@mail.ru>
2021-02-22 13:13:06 +0300

    Fixed changeAddToWishListStatus in DiscoverCollectionViewCell

--- Kmeza/View Controllers/Home Screen/Discover/DiscoverCollectionViewCell/DiscoverCollectionViewCell.swift
+++ Kmeza/View Controllers/Home Screen/Discover/DiscoverCollectionViewCell/DiscoverCollectionViewCell.swift
@@ -37,15 +37,18 @@ class DiscoverCollectionViewCell: UICollectionViewCell {
 	}
 	
 	func changeAddToWishListStatus() {
-		if viewModel.isAddedToWishList.value {
-			viewModel.changeAddToWishListStatus()
-			addToWishListButton.setImage(UIImage(named: "AddToWishList_White"), for: .normal)
-			print(false)
-		} else {
-			viewModel.changeAddToWishListStatus()
-			addToWishListButton.setImage(UIImage(named: "AddToWishListPressed_White"), for: .normal)
-			print(true)
+		viewModel.isAddedToWishList.bind { [unowned self] isAddedToWishList in
+			self.isAddedToWishList = isAddedToWishList
 		}
+
+		addToWishListButton.setImage(changeImageForAddwishListButton(value: isAddedToWishList),
+									 for: .normal)
+
+		viewModel.changeAddToWishListStatus()
+	}
+	
+	private func changeImageForAddwishListButton(value: Bool) -> UIImage {
+		UIImage(named: value ? "AddToWishList_White" : "AddToWishListPressed_White")!
 	}
 	
 	private func configureUI() {
